e02db0a1b48978cd2dcf4e4b6e463444e0e8bbaa
Haoyu Xu <haoyxu@microsoft.com>
2019-03-06 11:36:52 +0800

    fix data file name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name = 'errant',
     version = 'v1.0',
     packages = ['errant'],
-    package_data = {'errant':['resources/en_GB-large.txt','resources/en-ptb-map']},
+    package_data = {'errant':['resources/en_GB-large.txt','resources/en-ptb_map']},
     install_requires = [
         'spacy==1.9.0',
         'nltk==3.3'
